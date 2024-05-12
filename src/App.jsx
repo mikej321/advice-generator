8dcf70935d43ffaf41b18692302e85dfafa6e8be
Michael Johnson <michaeljcantley@gmail.com>
2024-05-12 12:42:38 -0400

    Finish mobile & desktop styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import patternMobile from './images/pattern-divider-mobile.svg';
+import patternDesktop from './images/pattern-divider-desktop.svg';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [userWidth, setUserWidth] = useState(document.body.clientWidth);
+  let url = "https://api.adviceslip.com/advice";
+
+    /* Json object has 2 properties,
+       id & advice
+     */
+
+  useEffect(() => {
+    const handleResize = () => {
+      setUserWidth(document.body.clientWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [userWidth])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Card userSpec={userWidth} />
+    </div>
   )
 }
 
+function Card({ advice, userSpec }) {
+
+  return (
+    <div className="card_container">
+      <h1 className="advice_title">Advice <span className="advice_num">#117</span></h1>
+      <p className="advice_quote">It is easy to sit up and take notice, what's difficult is
+        getting up and taking action.
+      </p>
+      {
+        userSpec <= 600 ? <img className="divider_mobile" src={patternMobile} /> : <img className="divider_desktop" src={patternDesktop} />
+      }
+      <div className="dice_container">
+        <svg className="dice" width="24" height="24" viewBox="-4 -3 30 30" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
+      </div>
+    </div>
+  )
+}
+ 
 export default App
